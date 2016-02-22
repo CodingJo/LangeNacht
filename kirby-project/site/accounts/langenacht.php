0053661dbd3ef65d69b516f7e93dc3f0aef253b4
CodingJo <jonathan.dudek@web.de>
2016-02-22 20:20:03 +0100

    first commit

---
+++ kirby-project/site/accounts/langenacht.php
@@ -0,0 +1,8 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: langenacht
+email: jonathan.dudek@web.de
+password: >
+  $2a$10$ctb8DVyRZxRu71z3Yo0WF.oVTzKgfw9GodJzdRgTwBj5p6Nih79OO
+language: de
+role: admin
